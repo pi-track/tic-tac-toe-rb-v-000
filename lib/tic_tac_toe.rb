7a5fa6fa1e14c02d4ccc1c097aff39f681cc6784
Patrick Eells <patrick.eells@gmail.com>
2018-01-05 16:12:51 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,68 @@
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+    [0,1,2],  # Top row
+    [3,4,5],  # Middle row
+    [6,7,8],  # Bottom row
+    [0,3,6],  # 1st column
+    [1,4,7],  # 2nd column
+    [2,5,8],  # 3rd column
+    [0,4,8],  # diagonal down
+    [2,4,6]   # diagonal up
+]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i-1
+end
+
+def move(array,index,value = "X")
+  array[index] = value
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+# code your #valid_move? method here
+def valid_move?(board, index)
+  if position_taken?(board, index) == true
+    false
+  elsif index > board.size-1
+    false
+  else
+    true
+  end
+end
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combo|
+    positions = [board[combo[0]],board[combo[1]],board[combo[2]]]
+      if (positions[0] == "X" && positions[1] == "X" && positions[2] == "X") || (positions[0] == "O" && positions[1] == "O" && positions[2] == "O")
+        return combo
+      end
+  end
+  return false
+end
+
+def full?(board)
+  board.all? {|position| !(position.nil? || position == " ")}
+end
+
+def draw?(board)
+  (won?(board) == false && full?(board) == true)? true : false
+end
+
+def over?(board)
+  (won?(board) != false || draw?(board) == true)? true : false
+end
+
+def winner(board)
+  won?(board) == false ?  nil : board[won?(board)[0]]
+end
